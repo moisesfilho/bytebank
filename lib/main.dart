b2957fc866481e7cc7d6e0f74954b08b138d3a45
NOTE-MOISES\moisesfilho <moises.son@gmail.com>
2020-01-25 21:38:17 -0300

    Montando estrutura base da interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Column(
-      children: <Widget>[
-        Text(
-          'Deliver features faster',
-          textDirection: TextDirection.ltr,
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: ListaTranferencias(),
+        appBar: AppBar(
+          title: Text('Transferências'),
         ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
         ),
-        Column(
-          children: <Widget>[
-            Text(
-              'Craft beautiful UIs',
-              textDirection: TextDirection.ltr,
-            ),
-          ],
-        )
-      ],
+      ),
     ));
+
+class ListaTranferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTranferencia(Tranferencia(100.0, 1234)),
+        ItemTranferencia(Tranferencia(101.0, 1234)),
+        ItemTranferencia(Tranferencia(102.0, 1234))
+      ],
+    );
+  }
+}
+
+class ItemTranferencia extends StatelessWidget {
+  final Tranferencia _tranferencia;
+
+  ItemTranferencia(this._tranferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_tranferencia.valor.toString()),
+      subtitle: Text(_tranferencia.numeroConta.toString()),
+    ));
+  }
+}
+
+class Tranferencia {
+  final double valor;
+  final int numeroConta;
+
+  Tranferencia(this.valor, this.numeroConta);
+}
